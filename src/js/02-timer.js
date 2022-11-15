450ed13fb997c2fb4fd849a5cbc6915e61dfb01b
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2022-11-15 01:52:46 +0100

    2 task and part of 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,24 +1,133 @@
 // flatpickr and notiflix.
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix';
+// console.log(Notify)
 
-const userDate = document.querySelector('#datetime-picker');
+const refs = {
+    userDate: document.querySelector('#datetime-picker'),
+    btnEl: document.querySelector('[data-start]'),
+    timerContainer: document.querySelector('.timer'),
+    field: document.querySelector('.field'),
+    daysEl: document.querySelector('[data-days]'),
+    hoursEl: document.querySelector('[data-hours]'),
+    minutesEl: document.querySelector('[data-minutes]'),
+    secondsEl: document.querySelector('[data-seconds]'),
+} 
+// console.log(refs);
+
+
+
+
+// style
+refs.btnEl.style.backgroundColor = '#00ccff';
+refs.btnEl.style.borderColor = '#e9e22b';
+refs.timerContainer.style.display = 'flex';
+refs.timerContainer.style.margin = '50px';
+refs.timerContainer.style.gap = '30px';
+refs.timerContainer.style.justifyContent = 'center';
+refs.timerContainer.style.alignItems = 'center';
+refs.timerContainer.style.color = '#2b97e9';
+refs.timerContainer.style.textShadow = '0 6px #e9e22b';
+refs.timerContainer.style.fontSize = '31px';
+refs.field.style.gap = '30px';
+
+// const currentTime = new Date();
+// const dayFromInput = currentTime.getDay();
+// console.log(dayFromInput);
+// const hourFromInput = currentTime.getHours();
+// console.dir(hourFromInput);
+// const minutesFromInput = currentTime.getMinutes();
+// // console.log(minutesFromInput);
+// const secondsFromInput = currentTime.getSeconds();
+// // console.log(secondsFromInput);
+
 
 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        //   console.log(selectedDates[0]);
+        //   загальний
+        const newData = selectedDates[0];
+        console.log(newData);
+        const currentTime = Date.now();
+        console.log(currentTime);
+        if (newData <= currentTime) {
+            refs.btnEl.disabled = true;
+            Notify.failure("Please choose a date in the future");
+            return;
 
-flatpickr(userDate, options);
+        }
+        refs.btnEl.disabled = false;
+        refs.btnEl.addEventListener('submit', () => {
+            
+            refs.btnEl.disabled = true;
+            let id = null;
+            id = setInterval(() => {
+                
+                let timeMs = newData - currentTime;
+                console.log(timeMs);
+                const timeOnDisplay = convertMs(timeMs);
+                console.log(timeOnDisplay);
+                refs.daysEl.textContent = addLeadingZero(timeOnDisplay.days);
+                refs.hoursEl.textContent = addLeadingZero(timeOnDisplay.hours);
+                refs.minutesEl.textContent = addLeadingZero(timeOnDisplay.minutes);
+                refs.secondsEl.textContent = addLeadingZero(timeOnDisplay.seconds);
+
+                if (timeOnDisplay === '0') {
+                    clearInterval(timerID);
+                    Notify.info("The end");
+                    return
+                }
+            }, 1000)
+       
+        })
+    }
+}
+
+    
+
+const flatpicker =  flatpickr(refs.userDate, options);
+
+// function updateClockface({ hours, mins, secs }) {
+//     refs.clockface.textContent = `${hours} : ${mins} : ${secs}`;
+// }
+// const timer = new Timer({
+//     onTick: updateClockface
+//     });
 
 
 
+function convertMs(timeMs) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+
+  // Remaining days
+  const days = Math.floor(timeMs / day);
+  // Remaining hours
+  const hours = Math.floor((timeMs % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((timeMs % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((timeMs % day) % hour) % minute) / second);
+    console.log('HELLO')
+    return { days, hours, minutes, seconds };
+    
+      
+}
+
+
+function addLeadingZero(value) {
+    return value.toString().padStart(2, "0")
+}
 
 
 
@@ -30,8 +139,3 @@ flatpickr(userDate, options);
 // });
 
 // console.log(flatpickr);
-
-// із завдання
-// if (currentDate < selectedDates) {
-//     window.alert("Please choose a date in the future");
-//  }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// HTML містить розмітку форми, в поля якої користувач буде вводити першу затримку в мілісекундах, 
+// крок збільшення затримки для кожного промісу після першого і кількість промісів, яку необхідно створити.
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. 
+// Під час кожного виклику передай їй номер промісу(position), що створюється, і затримку, враховуючи першу затримку(delay), 
+// введену користувачем, і крок(step).
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. 
+// Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. 
+
+import { Notify } from 'notiflix';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmitFormEl);
+
+// function
+function onSubmitFormEl(evt) {
+  evt.preventDefault();
+  const { step, delay, amount } = evt.target.elements;
+  const btn = evt.target.lastElementChild;
+  
+  let totalDelay = Number(delay.value);
+
+  btn.disabled = true;
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, totalDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    totalDelay += Number(step.value);
   }
+   // add disable
+  setTimeout(() => {
+    btn.disabled = false;
+  }, totalDelay);
+}
+
+// promise
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+});
 }
